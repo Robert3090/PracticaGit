87a23946138cf12bf0701998ac20b388463b8490
Robert3090 <b-nser@hotmail.com>
2026-04-02 00:40:11 -0600

    prueba SSH

--- Bienvenido.java
+++ Bienvenido.java
@@ -2,5 +2,6 @@ public class Bienvenido {
     public static void main(String[] args) {
         System.out.println("¡Bienvenido a Java!");
 	System.out.println("Mensaje de nuestro primer commit");
+	System.out.println("Mensaje para probar subir con ssh");
     }
 }
